e79958b6e77b9fde78dcc179b66ca575f4101775
Keith <kmcl1321@gmail.com>
2018-03-03 23:07:40 +0000

    added looping path behaviour

---
+++ Assets/Scripts/BasicPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicPath : MonoBehaviour {
+
+    public List<Vector3> waypoints = new List<Vector3>();
+    public int next = 0;
+    
+    void Start()
+    {
+        waypoints.Clear();
+        int count = transform.childCount;
+        for (int i = 0; i < count; i++)
+        {
+            waypoints.Add(transform.GetChild(i).position);
+        }
+    }
+
+    public Vector3 NextWaypoint()
+    {
+        return waypoints[next];
+    }
+
+    public void AdvanceToNext()
+    {
+        next = (next + 1) % waypoints.Count;
+    }
+}
---
+++ Assets/Scripts/FollowBasicPath.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowBasicPath : SteeringBehaviour
+{
+    public BasicPath path;
+    Vector3 nextWaypoint;
+
+    public override Vector3 Calculate()
+    {
+        nextWaypoint = path.NextWaypoint();
+        if (Vector3.Distance(transform.position, nextWaypoint) < 100)
+        {
+            path.AdvanceToNext();
+        }
+        
+        return boid.SeekForce(nextWaypoint);
+    }
+}
