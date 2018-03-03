9684566dca3df5b8f486eae706710b7f1b79d593
Keith <kmcl1321@gmail.com>
2018-03-03 23:21:32 +0000

    added chase behaviour

---
+++ Assets/Scripts/ChaseTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseTarget : SteeringBehaviour {
+
+    public Boid target;
+    Vector3 targetPos;
+
+    public override Vector3 Calculate()
+    {
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+        float time = dist / boid.maxSpeed;
+
+        targetPos = target.transform.position + (time * target.velocity);
+
+        return boid.SeekForce(targetPos);
+    }
+}
